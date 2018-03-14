93cb83bee99aa71c437fcd26a9563a003305dda6
Elizabeth Wong <lizzwong@gmail.com>
2018-03-14 15:42:24 -0500

    two inputs

--- local-state/src/components/App/App.js
+++ local-state/src/components/App/App.js
@@ -6,10 +6,39 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {user: 'Banana'};
+    this.state = {
+      user: {
+          name: 'Elizabeth',
+          city: 'Ames',
+    }
+  };
+    this.handleChange = this.handleChange.bind(this);
+
 
   }
 
+  handleChange(event) {
+    console.log(event.target.value);
+   
+    const name = event.target.name;
+
+   if (name === 'person') {
+    this.setState( {user: {...this.state.user, name: event.target.value}});
+   }
+
+   else {
+     console.log(event.target.value);
+     this.setState({ user: { ...this.state.user, city: event.target.value } });
+
+   }
+
+  }
+
+  // taco(event) {
+  //   console.log(event.target.value);
+  //   this.setState({ user: { ...this.state.user, city: event.target.value } });
+
+  // }
   render() {
     return (
       <div className="App">
@@ -17,7 +46,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-       <p>{this.state.user}</p>
+        <input name="person"  onChange={this.handleChange}/>
+        <input name="city" onChange={this.handleChange}/>
+       <p>{this.state.user.name} is from {this.state.user.city}</p>
       </div>
     );
   }
